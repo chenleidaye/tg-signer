8b8cf556048da769d9dd498950bca5d39fe3e4bf
ahisna <138838948+chenleidaye@users.noreply.github.com>
2025-06-01 11:27:21 +0800

    Update app.py

--- docker/app.py
+++ docker/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, tg-signer!'
+    return "Hello tg-signer!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)  # 确保监听0.0.0.0，端口8080
